2779f61621fe3e79c1f7662f96528a2d4ad152b1
mengwenliang <mengwenliang@imiaoke.cn>
2018-12-08 12:16:27 +0800

    add project

---
+++ domain/src/main/java/com/orb/domain/DomainApplication.java
@@ -0,0 +1,12 @@
+package com.orb.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomainApplication.class, args);
+    }
+}
